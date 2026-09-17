1cbc2b48b3804beb7ff05dfa71e4b40f0ab232b8
Esenzhanov Kubanychbek <gloompi@gmail.com>
2026-09-18 00:22:06 +0200

    Add: top-k-elements-in-list - submission-1

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-1.kt
@@ -0,0 +1,23 @@
+class Solution {
+    fun topKFrequent(nums: IntArray, k: Int): IntArray {
+        val freq = Array(nums.size) { mutableListOf<Int>() }
+
+        nums.toList().groupingBy { it }.eachCount().forEach { (num, count) -> freq[count-1].add(num) }
+
+        var filled = 0
+        val res = mutableListOf<Int>()
+
+        for (bucket in freq.reversed()) {
+            for (num in bucket) {
+                res.add(num)
+                filled++
+
+                if (filled == k) {
+                    return res.toIntArray()
+                }
+            }
+        }
+
+        error("No solution")
+    }
+}
