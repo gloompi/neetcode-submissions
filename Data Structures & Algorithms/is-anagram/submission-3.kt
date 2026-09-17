0e3292ae53bc4c6c19ba49421f861218cd9a6027
Esenzhanov Kubanychbek <gloompi@gmail.com>
2026-09-17 19:43:42 +0200

    Add: is-anagram - submission-3

---
+++ Data Structures & Algorithms/is-anagram/submission-3.kt
@@ -0,0 +1,13 @@
+class Solution {
+    fun isAnagram(s: String, t: String): Boolean {
+        if (s.length != t.length) return false
+        val counts = IntArray(26)
+
+        for (i in s.indices) {
+            counts[s[i] - 'a']++
+            counts[t[i] - 'a']--
+        }
+
+        return counts.all { it == 0 }
+    }
+}
