cba6724c480160330d10079bde75c7b7973e88bd
Esenzhanov Kubanychbek <gloompi@gmail.com>
2026-09-21 12:50:35 +0200

    Add: majority-element - submission-2

---
+++ Data Structures & Algorithms/majority-element/submission-2.kt
@@ -0,0 +1,10 @@
+class Solution {
+    fun majorityElement(nums: IntArray): Int {
+        return nums
+            .toList()
+            .groupingBy { it }
+            .eachCount()
+            .maxBy { it.value }
+            .key
+    }
+}
